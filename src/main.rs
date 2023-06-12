dc9bbb899c717dcdcc1f984af5bbe2caf596acd4
pavles6 <pavles6@protonmail.com>
2023-06-12 17:44:18 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        return self.part;
+    }
+}
